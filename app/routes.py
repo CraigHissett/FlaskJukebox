d63045ea1f52b2c3e59048fa85e0d890aaa747c1
Craig Hissett <craighissett@gmail.com>
2018-04-03 23:53:35 +0100

    Added app.db and migration info

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,7 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
